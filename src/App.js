aabcdbe3f346ea7e4c49602890043dbce084753d
Mehdi <mehdibouzidi@live.nl>
2019-07-31 22:27:19 +0200

    v2
    Bootstrap removed

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import './App.scss';
 function App() {
   return (
     <div className="container">
-      <div className="row">
-        <Card title="Digital content & Advertising"/>
-        <Card title="Strategy & Consulting"/>
-        <Card title="Branding"/>
-        <Card title="Product & Business innovation"/>
-        <Card title="e-Commerce"/>
-        <Card title="Customer Experience & Design"/>
-      </div>
+      <Card title="Digital content & Advertising"/>
+      <Card title="Strategy & Consulting"/>
+      <Card title="Branding"/>
+      <Card title="Product & Business innovation"/>
+      <Card title="e-Commerce"/>
+      <Card title="Customer Experience & Design"/>
     </div>
   )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,14 +3,14 @@ import React from 'react';
 {/* The Card functions */}
 function Card({title}){
     return(
-         <div className="col-md-4">
-            <div className="wrapper">
+        <div className="wrapper">
+            <div className="card">
                 <h5 className="title">{title}</h5>
                 <p className="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
                 <div>
                     <span className="learn_more"> Learn More </span>
-                        <svg viewBox="0 0 100 40" class="_3d0uN3bbtLgJbBB_pOtvBY" className="arrow_right">
+                        <svg viewBox="0 0 100 40" className="arrow_right">
                             <line x1="2.584" y1="20" x2="92.362" y2="20"></line>
                             <polyline points="77.438,5.076 92.362,20 77.438,34.924"></polyline>
                         </svg>
